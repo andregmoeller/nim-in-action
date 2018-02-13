595d18a4e36dd305b6e7943732220edfa467d7bd
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-02-13 14:57:14 +0100

    Generates a JSON representation of a Message

--- chapter-3/chat-app/src/protocol.nim
+++ chapter-3/chat-app/src/protocol.nim
@@ -10,21 +10,31 @@ proc parseMessage*(data: string): Message =
     result.username = dataJson["username"].getStr()
     result.message = dataJson["message"].getStr()
 
+proc createMessage*(username, message: string): string =
+    result = $(%{
+        "username": %username,
+        "message": %message
+    }) & "\c\l"
+
 when isMainModule:
-    # Test valid JSON
+    # Test parsing of valid JSON
     block:
         let data = """{"username": "John", "message": "Hi!"}"""
         let parsed = parseMessage(data)
         doAssert parsed.username == "John"
         doAssert parsed.message == "Hi!"
 
-    # Test invalid JSON
+    # Test parsing of invalid JSON
     block:
-        let data = "foobar"
         try:
-            discard parseMessage(data)
+            discard parseMessage("foobar")
             doAssert false
         except JsonParsingError:
             doAssert true
         except:
             doAssert false
+    
+    # Test generating a JSON
+    block:
+        let expected = """{"username":"dom","message":"hello"}""" & "\c\l"
+        doAssert createMessage("dom", "hello") == expected
