ac6b88f410679a83931fbd7dc2eb9629b4a9876c
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-02-13 22:13:41 +0100

    Connects to server

--- chapter-3/chat-app/src/client.nim
+++ chapter-3/chat-app/src/client.nim
@@ -1,4 +1,15 @@
-import os, threadpool
+import os, threadpool, asyncdispatch, asyncnet
+import protocol
+
+proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
+    echo("Connecting to ", serverAddr)
+    await socket.connect(serverAddr, 7687.Port)
+    echo("Connected!")
+
+    while true:
+        let line = await socket.recvLine()
+        let parsed = parseMessage(line)
+        echo(parsed.username, " said ", parsed.message)
 
 echo("Chat application started")
 
@@ -6,7 +17,9 @@ if paramCount() == 0:
     quit("Please specify the server address, e.g. ./client localhost")
 
 let serverAddr = paramStr(1)
-echo("Connecting to", serverAddr)
+
+var socket = newAsyncSocket()
+asyncCheck connect(socket, serverAddr)
 
 while true:
     let message = spawn stdin.readLine()
