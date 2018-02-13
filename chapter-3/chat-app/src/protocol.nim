30e2d7ee3906fa8ebd841e8333404accf4ebdf70
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-02-13 14:51:52 +0100

    Converts JSON to Messages

--- chapter-3/chat-app/src/protocol.nim
+++ chapter-3/chat-app/src/protocol.nim
@@ -1,7 +1,30 @@
+import json
+
 type
     Message* = object
         username*: string
         message*: string
 
 proc parseMessage*(data: string): Message =
-    discard
+    let dataJson = parseJson(data)
+    result.username = dataJson["username"].getStr()
+    result.message = dataJson["message"].getStr()
+
+when isMainModule:
+    # Test valid JSON
+    block:
+        let data = """{"username": "John", "message": "Hi!"}"""
+        let parsed = parseMessage(data)
+        doAssert parsed.username == "John"
+        doAssert parsed.message == "Hi!"
+
+    # Test invalid JSON
+    block:
+        let data = "foobar"
+        try:
+            discard parseMessage(data)
+            doAssert false
+        except JsonParsingError:
+            doAssert true
+        except:
+            doAssert false
