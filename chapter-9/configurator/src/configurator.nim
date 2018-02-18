7ab8e4feca19f2b57ea96c742b76f82811d53dba
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-02-19 00:17:26 +0100

    Generates the AST for a type definition

--- chapter-9/configurator/src/configurator.nim
+++ chapter-9/configurator/src/configurator.nim
@@ -1,7 +1,34 @@
 import macros
 
-dumpTree:
-    type
-        MyAppConfig = ref object
-            address: string
-            port: int
+proc createRefType(ident: NimIdent, identDefs: seq[NimNode]): NimNode =
+    result = newTree(nnkTypeSection,
+        newTree(nnkTypeDef,
+            newIdentNode(ident),
+            newEmptyNode(),
+            newTree(nnkRefTy,
+                newTree(nnkObjectTy,
+                    newEmptyNode(),
+                    newEmptyNode(),
+                    newTree(nnkRecList, identDefs)
+                )
+            )
+        )
+    )
+
+proc toIdentDefs(stmtList: NimNode): seq[NimNode] =
+    expectKind(stmtList, nnkStmtList)
+    result = @[]
+
+    for child in stmtList:
+        expectKind(child, nnkCall)
+        result.add(newIdentDefs(child[0], child[1][0]))
+
+macro config(typeName: untyped, fields: untyped): untyped =
+    let identDefs = toIdentDefs(fields)
+    result = newStmtList()
+    result.add createRefType(typeName.ident, identDefs)
+    echo treeRepr(result)
+
+config MyAppConfig:
+    address: string
+    port: int
