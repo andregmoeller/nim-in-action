dac132c18c46deabecd5c4327274f0973eaa9417
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-02-19 00:22:31 +0100

    Generates the constructor procedure

--- chapter-9/configurator/src/configurator.nim
+++ chapter-9/configurator/src/configurator.nim
@@ -23,11 +23,18 @@ proc toIdentDefs(stmtList: NimNode): seq[NimNode] =
         expectKind(child, nnkCall)
         result.add(newIdentDefs(child[0], child[1][0]))
 
+template constructor(ident: untyped): untyped =
+    proc `new ident`(): `ident` =
+        new result
+
 macro config(typeName: untyped, fields: untyped): untyped =
     let identDefs = toIdentDefs(fields)
     result = newStmtList()
     result.add createRefType(typeName.ident, identDefs)
+    result.add getAst(constructor(typeName.ident))
+
     echo treeRepr(result)
+    echo repr(result)
 
 config MyAppConfig:
     address: string
